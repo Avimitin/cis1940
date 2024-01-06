6303d9280258b03dfa73724c6bc94190cae4672f
Avimitin <dev@avimit.in>
2024-01-06 18:41:57 +0800

    03-rec-poly: add exercise2
    Signed-off-by: Avimitin <dev@avimit.in>

--- src/03-rec-poly/Golf.hs
+++ src/03-rec-poly/Golf.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 
-module Golf (skips) where
+module Golf (skips, localMaxima) where
 
 skips :: [a] -> [[a]]
 skips inputs =
@@ -8,3 +8,7 @@ skips inputs =
         selectNthElement nth = map snd . filter (\(i, _) -> i `mod` nth == 0) . zip [1 ..]
      in
         [selectNthElement i inputs | i <- [1 .. length inputs]]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x : y : z : xs) = [y | y > x && y > z] ++ localMaxima (y : z : xs)
+localMaxima _ = []
